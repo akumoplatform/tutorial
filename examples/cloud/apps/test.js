c47bee2ad3088226e427f682a777db027bcce7e4
apedrina <pedrina.alisson@gmail.com>
2020-12-05 12:19:07 +0000

    test

--- examples/cloud/apps/test.js
+++ examples/cloud/apps/test.js
@@ -1,9 +1,29 @@
-Cmds.wait(true);
-let pids = Cmds.pids(['config.jar','registry.jar','gateway.jar','customer.jar']);
+let config = {
+    "src": "/Users/alissonpedrina/Documents/gitprojects/platform/native-cloud/",
+    "configSrc": "/Users/alissonpedrina/Documents/gitprojects/platform/native-cloud/config",
+    "registrySrc": "/Users/alissonpedrina/Documents/gitprojects/platform/native-cloud/registry",
+    "customerSrc": "/Users/alissonpedrina/Documents/gitprojects/platform/native-cloud/customer",
+    "gatewaySrc": "/Users/alissonpedrina/Documents/gitprojects/platform/native-cloud/gateway"
+}
 
-pids.forEach(pid => {
-    console.log('killing: ' + pid);
-    
-});
+params = {
+    'args': ['mvn', 'clean', 'install', '-DskipTests'],
+    'base': config.src,
+    'wait': false,
+    'bind': false,
+    'out': 'test.txt'
+}
+let p = cmd(params).run();
 
-Cmds.kill(pids);
+let count = 0;
+while (true) {
+    if (Files.count('test.txt') > count) {
+        console.log(Files.line('test.txt', count));
+        count += 1;
+
+    }
+    if (p.isStopped()) {
+        break;
+    }
+}
+
